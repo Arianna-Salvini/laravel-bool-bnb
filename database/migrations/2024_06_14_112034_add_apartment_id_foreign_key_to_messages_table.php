459c3824a282c13194d88a06f3dc1fc080e9ce94
alessiavozzo <a.vozzo95@gmail.com>
2024-06-14 13:32:57 +0200

    add apartment id column to messages table

---
+++ database/migrations/2024_06_14_112034_add_apartment_id_foreign_key_to_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->unsignedBigInteger('apartment_id');
+            $table->foreign('apartment_id')
+            ->references('id')
+            ->on('apartments')
+            ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign('messages_apartment_id_foreign');
+            $table->dropColumn('apartment_id');
+        });
+    }
+};
