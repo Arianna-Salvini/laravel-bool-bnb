436578959b527b57e134f6b4244b0981f45a20f3
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-14 17:48:05 +0200

    add create in controller and in views

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,71 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
-    
-<section id="apartment_table">
+    <section id="apartment_table">
 
 
-    <div class="container mt-5">
-        <h2>All Apartments</h2>
-        <div
-        class="table-responsive mt-3"
-    >
-        <table
-            class="table table-primary"
-        >
-            <thead>
-                <tr class="text-center">
-                    <th scope="col">ID</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Address</th>
-                    <th scope="col">Visible</th>
-                    <th scope="col">Actions</th>
+        <div class="container mt-5">
+            <h2>All Apartments</h2>
+            <a href="{{ route('admin.apartments.create') }}" class="btn btn-primary">Insert New Apartment</a>
+            <div class="table-responsive mt-3">
+                <table class="table table-primary">
+                    <thead>
+                        <tr class="text-center">
+                            <th scope="col">ID</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Visible</th>
+                            <th scope="col">Actions</th>
 
 
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($apartments as $apartment)
-                <tr class="align-middle">
-                    
-                        
-                    <td>{{ $apartment->id }}</td>
-                    <td><img src="{{ $apartment->image }}" alt="" width="100"></td>
-                    <td>{{ $apartment->title }}</td>
-                    <td>{{ $apartment->address }}</td>
-                    <td>
-                        @if ($apartment->visibility)
-                        
-                        <i class="fa-solid fa-check"></i>
-                        @else
-                        <i class="fa-solid fa-x"></i>
-                        @endif
-                        
-                    </td>
-                    <td>
-                        
-                        <a class="btn btn-dark btn-sm " href="#" role="button"> <i class="fa fa-eye" aria-hidden="true"></i></a>
-                        <a class="btn btn-warning btn-sm " href="#" role="button"> <i class="fa fa-pencil" aria-hidden="true"></i></a>
-                        <a class="btn btn-danger btn-sm " href="#" role="button"> <i class="fa fa-trash" aria-hidden="true"></i></a>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($apartments as $apartment)
+                            <tr class="align-middle">
+
+
+                                <td>{{ $apartment->id }}</td>
+                                <td><img src="{{ $apartment->image }}" alt="" width="100"></td>
+                                <td>{{ $apartment->title }}</td>
+                                <td>{{ $apartment->address }}</td>
+                                <td>
+                                    @if ($apartment->visibility)
+                                        <i class="fa-solid fa-check"></i>
+                                    @else
+                                        <i class="fa-solid fa-x"></i>
+                                    @endif
+
+                                </td>
+                                <td>
+
+                                    <a class="btn btn-dark btn-sm " href="#" role="button"> <i class="fa fa-eye"
+                                            aria-hidden="true"></i></a>
+                                    <a class="btn btn-warning btn-sm " href="#" role="button"> <i
+                                            class="fa fa-pencil" aria-hidden="true"></i></a>
+                                    <a class="btn btn-danger btn-sm " href="#" role="button"> <i class="fa fa-trash"
+                                            aria-hidden="true"></i></a>
 
 
 
 
-                    </td>
-               
-                    @empty
-                        <span>No Apartments.</span>
-                    @endforelse
-                </tr> 
-                
-            </tbody>
-        </table>
-    </div>
-    </div>
-    
-</section>
+                                </td>
 
-{{-- @dd($apartments) --}}
+                            @empty
+                                <span>No Apartments.</span>
+                        @endforelse
+                        </tr>
 
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </section>
+
+    {{-- @dd($apartments) --}}
 @endsection
