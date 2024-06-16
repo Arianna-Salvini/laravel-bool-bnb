46d43c1d2aa44f0aa422136bbd41932b88bcb1e6
alessiavozzo <a.vozzo95@gmail.com>
2024-06-16 13:31:56 +0200

    edit profile page and routes not to show profile-data edit possibility
    -client didn't pay for the feature

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(function () {
@@ -27,7 +27,7 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    /* Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); */
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
