9cf4202b783fd0c165899798396a7046aaff1e87
alessiavozzo <a.vozzo95@gmail.com>
2024-06-17 21:18:15 +0200

    add comments

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,80 +9,67 @@ import.meta.glob([
 
 
 
-
+/* get all address inputs from document */
 let address = document.getElementById('address');
-
 let street_number = document.getElementById('street_number');
 let zip_code = document.getElementById('zip_code');
 let city = document.getElementById('city');
 let country_code = document.getElementById('country_code');
-console.log(address, street_number);
+//console.log(address, street_number);
 
+/* get list element from document (this is where I'll show address suggestions) */
 let addressList = document.getElementById('address-list');
+
+/* save api_key */
 let api_key = 'TubXmNyzFnYoGMpgu1RAnYEHnVO24pfI';
-/* 
-let inputs = [];
-inputs.push(address, street_number, zip_code, city, country_code);
-console.log(inputs);
 
-inputs.forEach((input, index) => {
-    //console.log(input);
-    input.addEventListener('input', function () {
-        let inputValue = this.value.replace(' ', '%20');
-
-        let url = `https://api.tomtom.com/search/2/search/${inputValue[1]}%20${inputValue[2]}%20${inputValue[3]}%20${inputValue[4]}?countrySet=${inputValue[5]}.json?view=Unified&relatedPois=off&key=${api_key}`;
-
-        async function getResults(url) {
-            let result = await fetch(url);
-            return result.json();
-        }
-
-        getResults(url).then(response => {
-            console.log(response);
-            addressList.innerHTML = '';
-            for (let i = 0; i < 5; i++) {
-                let resultAddress = document.createElement('li');
-                resultAddress.innerHTML = response.results[i].address.freeformAddress
-                addressList.appendChild(resultAddress);
-            }
-        });
-    })
-}) */
-
+/* add event listener on address input. Event to listen for: input (keyup) */
 address.addEventListener('input', function () {
-    console.log(this.value);
+    //console.log(this.value);
+
+    /* replace spaces with %20 */
     let addressValue = this.value.replace(' ', '%20');
 
-
+    /* create url using search api */
     let url = `https://api.tomtom.com/search/2/search/${addressValue}.json?view=Unified&relatedPois=off&key=${api_key}`;
 
+    /* use async function to get result: the function returns a promise resolved with result data transformed in json */
     async function getResults(url) {
         let result = await fetch(url);
         return result.json();
     }
 
+    /* after the promise is resolved, get json result */
     getResults(url).then(response => {
         console.log(response);
+
+        /* empty list */
         addressList.innerHTML = '';
+
+        /* show 5 suggestions. Create a list item for each loop */
         for (let i = 0; i < 5; i++) {
             let resultAddress = document.createElement('li');
 
+            /* write response full address into list item */
             resultAddress.innerHTML = response.results[i].address.freeformAddress
 
+            /* append list item to list */
             addressList.appendChild(resultAddress);
 
+            /* add event listener to each suggestion. Event to listen for: click */
             resultAddress.addEventListener('click', function () {
                 //console.log(this.innerHTML);
                 //console.log(address.value);
                 //console.log(response.results[i].address.streetName);
 
+                /* when I click, I get all fields autocomplete with response data */
                 address.value = response.results[i].address.streetName;
                 street_number.value = response.results[i].address.streetNumber;
                 zip_code.value = response.results[i].address.postalCode;
                 city.value = response.results[i].address.municipality;
                 country_code.value = response.results[i].address.countryCode;
 
-
+                /* when I select a suggestion, the other suggestions disappear */
                 addressList.innerHTML = '';
             })
         }
