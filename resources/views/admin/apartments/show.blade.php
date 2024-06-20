89f7adb7b3606eee0a0010901a2c480852f387a7
Kasketto001 <kaskettomaledetto@gmail.com>
2024-06-20 10:37:01 +0200

    Fixed Services and apartment details in show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container my-4 pb-3">
-        <div class="btn-actions d-flex justify-content-end mb-4">
-        <a class="btn btn-warning btn-sm btn-action" href="{{ route('admin.apartments.edit', $apartment) }}" role="button">
-            <i class="fa fa-pencil" aria-hidden="true"></i>
-        </a>
-        @include('partials.delete-apartments')
+    <div class="container mb-4 pb-3">
+        <div class="btn-actions d-flex justify-content-between align-items-center mb-4">
+            <div class="left">
+                <a class="btn btn-outline-secondary text-decoration-none my-3" href="{{ route('admin.apartments.index') }}"><i class="fa-solid fa-circle-left"></i> Go back</a>
+            </div>
+       <div class="right">
+            <a class="btn btn-warning btn-sm btn-action" href="{{ route('admin.apartments.edit', $apartment) }}" role="button">
+                <i class="fa fa-pencil" aria-hidden="true"></i>
+            </a>
+            @include('partials.delete-apartments')
+       </div>
         </div>
         <div class="row d-flex">
             <div class="col-md-8 mb-4">
-                <div class="card shadow-lg position-relative overflow-hidden rounded-5">
+                <div class="card position-relative overflow-hidden rounded-5">
                     @if ($apartment->image)
                         <div class="card-img-overlay d-flex align-items-center p-0">
                             <div class="w-100 h-100 bg-dark bg-opacity-25 p-4">
@@ -37,29 +42,55 @@
                         <h2>Details:</h2>
                     </div>
                     <div class="card-body">
-                        {{-- Address --}}
-                        <div class="mb-3">
-                            <strong>Address:</strong> {{ $apartment->address }}, {{ $apartment->street_number }}, {{ $apartment->zip_code }}
+                        <div class="d-flex flex-wrap gap-1">
+                            {{-- Address --}}
+                            <div class="mb-3">
+                                <span class="badge bg-dark rounded-5 p-2">
+                                    <strong><i class="fa-solid fa-map-pin"></i></strong> 
+                                    {{ $apartment->address }}, {{ $apartment->street_number }}, {{ $apartment->zip_code }}
+                                </span>
+                            </div>
+                        
+                            {{-- Apartment Details --}}
+                            <div class="mb-3">
+                                <span class="badge bg-dark rounded-5 p-2">
+                                    <strong><i class="fa-solid fa-ruler-combined"></i></strong> 
+                                    {{ $apartment->square_meters }} m²
+                                </span>
+                            </div>
+                            <div class="mb-3">
+                                <span class="badge bg-dark rounded-5 p-2">
+                                    <strong><i class="fa-solid fa-person-booth"></i></strong> 
+                                    {{ $apartment->rooms }} rooms
+                                </span>
+                            </div>
+                            <div class="mb-3">
+                                <span class="badge bg-dark rounded-5 p-2">
+                                    <strong><i class="fa-solid fa-bed"></i></strong> 
+                                    {{ $apartment->beds }} beds
+                                </span>
+                            </div>
+                            <div class="mb-3">
+                                <span class="badge bg-dark rounded-5 p-2">
+                                    <strong><i class="fa-solid fa-toilet"></i></strong> 
+                                    {{ $apartment->bathrooms }} bathrooms
+                                </span>
+                            </div>
+                        
+                            {{-- Visibility --}}
+                            <div class="mb-3">
+                                <span class="badge bg-dark rounded-5 p-2">
+                                    @if ($apartment->visibility == 0)
+                                        <i class="fa-solid fa-eye-slash"></i>
+                                        Not Visible
+                                    @else
+                                        <i class="fa-solid fa-eye"></i>
+                                        Visible
+                                    @endif
+                                </span>
+                            </div>
                         </div>
-
-                        {{-- Apartment Details --}}
-                        <div class="mb-3">
-                            <strong>Square Meters:</strong> {{ $apartment->square_meters }} m²
-                        </div>
-                        <div class="mb-3">
-                            <strong>Rooms:</strong> {{ $apartment->rooms }}
-                        </div>
-                        <div class="mb-3">
-                            <strong>Beds:</strong> {{ $apartment->beds }}
-                        </div>
-                        <div class="mb-3">
-                            <strong>Bathrooms:</strong> {{ $apartment->bathrooms }}
-                        </div>
-
-                        {{-- Visibility --}}
-                        <div class="mb-3">
-                            <strong>Visibility:</strong> {{ $apartment->visibility ? 'Visible' : 'Not Visible' }}
-                        </div>
+                        
 
                         {{-- Services --}}
                         <div class="mb-3">
@@ -80,14 +111,13 @@
                             <div class="icon">
                                 <i class="fa-solid fa-arrow-trend-up"></i>
                             </div>
-                            <span>Advertise your apartment!</span>
+                            <span class="text-fluid">Sponsor your apartment!</span>
                             <img class="img-button" src="https://i.ibb.co/rxZs9mM/04.png" alt="">
                         </div>
                     </a>
                 </div>
             </div>
         </div>
-        <a class="btn btn-outline-dark text-decoration-none my-3" href="{{ route('admin.apartments.index') }}">See All</a>
     </div>
 
     {{-- Script for map initialization --}}
