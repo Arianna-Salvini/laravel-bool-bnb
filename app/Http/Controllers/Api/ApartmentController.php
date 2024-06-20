253c360730f960ec55576237b6f70cd59100a25b
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-06-20 12:55:34 +0200

    Add range distance_dynamically change the range of research

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -12,24 +12,29 @@ class ApartmentController extends Controller
     public function index()
     {
         $apartments = Apartment::with('services', 'sponsorships', 'user')->orderByDesc('id')->paginate(10);
+
         return response()->json([
             'success' => true,
-            'results' => $apartments
+            'results' => $apartments,
         ]);
     }
 
-    public function searchApartments(Request $request){
+    public function searchApartments(Request $request)
+    {
 
-        $api_key= env('TOMTOM_API_KEY');
-        $base_api= 'https://api.tomtom.com/search/2/geocode/';
-        
-        $address= str_replace(' ', '%20', $request['address']);
+        $api_key = env('TOMTOM_API_KEY');
+        $base_api = 'https://api.tomtom.com/search/2/geocode/';
+
+        $address = str_replace(' ', '%20', $request['address']);
+
+        // Add range distance dynamically from range input in FE
+        $range_distance = $request->input('range_distance', 20);
 
         /* https://api.tomtom.com/search/2/geocode/Via%20degli%20anemoni%206%2000172%20Roma.json?storeResult=false&view=Unified&key=***** */
-        $api_url = $base_api . $address . '.json?storeResult=false&view=Unified&key=' . $api_key;        
+        $api_url = $base_api.$address.'.json?storeResult=false&view=Unified&key='.$api_key;
 
         /* if request has address filled */
-        if($request->has('address') && $request['address'] != ''){
+        if ($request->has('address') && $request['address'] != '') {
 
             /* call tomtom api and get results*/
             /* NB bypass ssl */
@@ -40,38 +45,44 @@ class ApartmentController extends Controller
             $coordinates = $result['position'];
             $latitude = $coordinates['lat'];
             $longitude = $coordinates['lon'];
-            
-            $earth_radius= 6371; //km
-            $range_radius= 20;    
-    
+
+            $earth_radius = 6371; //km
+            // $range_radius = 20;
+
             /* given lat and long to radians */
-            $rad_lat=deg2rad($latitude);
-            $rad_long=deg2rad($longitude);
-    
+            $rad_lat = deg2rad($latitude);
+            $rad_long = deg2rad($longitude);
+
             /* range_radius must be converted to radians before being converted to degrees */
-            $rad_radius= ($range_radius / $earth_radius);
+            // $rad_radius = ($range_radius / $earth_radius);
+
+            //use range_distance in order base on given range from user and calculate the radius
+            $rad_radius = ($range_distance / $earth_radius);
+
             /* convert range radius to degrees */
-            $deg_radius= rad2deg($rad_radius);
-    
+            $deg_radius = rad2deg($rad_radius);
+
             /* limits (googled)*/
-            $lat_min= ($latitude - $deg_radius);
-            $lat_max= ($latitude + $deg_radius);
-            $long_min= ($longitude - $deg_radius / cos($rad_lat));
-            $long_max= ($longitude + $deg_radius / cos($rad_lat));
+            $lat_min = ($latitude - $deg_radius);
+            $lat_max = ($latitude + $deg_radius);
+            $long_min = ($longitude - $deg_radius / cos($rad_lat));
+            $long_max = ($longitude + $deg_radius / cos($rad_lat));
 
             $query = Apartment::query();
-            $apartments = $query->whereBetween('latitude', [$lat_min, $lat_max])->whereBetween('longitude', [$long_min, [$long_max]])->with('services', 'sponsorships', 'user')->orderByDesc('id')->paginate(4);
-                                
-    
+            $apartments = $query->whereBetween('latitude', [$lat_min, $lat_max])
+                ->whereBetween('longitude', [$long_min, $long_max])
+                ->with('services', 'sponsorships', 'user')
+                ->orderByDesc('id')
+                ->paginate(4);
+
             return response()->json([
                 'success' => true,
-                'response' => $apartments
+                'response' => $apartments,
             ]);
-        }
-        else{
+        } else {
             return response()->json([
                 'success' => false,
-                'message' => 'Insert an address'
+                'message' => 'Insert an address',
             ]);
         }
 
