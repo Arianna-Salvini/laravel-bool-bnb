c6d837e9f1ea60a935433ab7be2ee2ac50a6db5b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-20 11:59:49 +0200

    add endpoint api for single apartment add show method

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -18,18 +18,40 @@ class ApartmentController extends Controller
         ]);
     }
 
-    public function searchApartments(Request $request){
+    public function show($slug)
+    {
+        $apartment = Apartment::with('services', 'sponsorships', 'user')->where('slug', $slug)->first();
 
-        $api_key= env('TOMTOM_API_KEY');
-        $base_api= 'https://api.tomtom.com/search/2/geocode/';
-        
-        $address= str_replace(' ', '%20', $request['address']);
+        if ($apartment) {
+            return response()->json(
+                [
+                    'success' => true,
+                    'response' => $apartment,
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    'success' => false,
+                    'response' => 'Sorry there is not your apartment',
+                ]
+            );
+        }
+    }
+
+    public function searchApartments(Request $request)
+    {
+
+        $api_key = env('TOMTOM_API_KEY');
+        $base_api = 'https://api.tomtom.com/search/2/geocode/';
+
+        $address = str_replace(' ', '%20', $request['address']);
 
         /* https://api.tomtom.com/search/2/geocode/Via%20degli%20anemoni%206%2000172%20Roma.json?storeResult=false&view=Unified&key=***** */
-        $api_url = $base_api . $address . '.json?storeResult=false&view=Unified&key=' . $api_key;        
+        $api_url = $base_api . $address . '.json?storeResult=false&view=Unified&key=' . $api_key;
 
         /* if request has address filled */
-        if($request->has('address') && $request['address'] != ''){
+        if ($request->has('address') && $request['address'] != '') {
 
             /* call tomtom api and get results*/
             /* NB bypass ssl */
@@ -40,40 +62,38 @@ class ApartmentController extends Controller
             $coordinates = $result['position'];
             $latitude = $coordinates['lat'];
             $longitude = $coordinates['lon'];
-            
-            $earth_radius= 6371; //km
-            $range_radius= 20;    
-    
+
+            $earth_radius = 6371; //km
+            $range_radius = 20;
+
             /* given lat and long to radians */
-            $rad_lat=deg2rad($latitude);
-            $rad_long=deg2rad($longitude);
-    
+            $rad_lat = deg2rad($latitude);
+            $rad_long = deg2rad($longitude);
+
             /* range_radius must be converted to radians before being converted to degrees */
-            $rad_radius= ($range_radius / $earth_radius);
+            $rad_radius = ($range_radius / $earth_radius);
             /* convert range radius to degrees */
-            $deg_radius= rad2deg($rad_radius);
-    
+            $deg_radius = rad2deg($rad_radius);
+
             /* limits (googled)*/
-            $lat_min= ($latitude - $deg_radius);
-            $lat_max= ($latitude + $deg_radius);
-            $long_min= ($longitude - $deg_radius / cos($rad_lat));
-            $long_max= ($longitude + $deg_radius / cos($rad_lat));
+            $lat_min = ($latitude - $deg_radius);
+            $lat_max = ($latitude + $deg_radius);
+            $long_min = ($longitude - $deg_radius / cos($rad_lat));
+            $long_max = ($longitude + $deg_radius / cos($rad_lat));
 
             $query = Apartment::query();
             $apartments = $query->whereBetween('latitude', [$lat_min, $lat_max])->whereBetween('longitude', [$long_min, [$long_max]])->with('services', 'sponsorships', 'user')->orderByDesc('id')->paginate(4);
-                                
-    
+
+
             return response()->json([
                 'success' => true,
                 'response' => $apartments
             ]);
-        }
-        else{
+        } else {
             return response()->json([
                 'success' => false,
                 'message' => 'Insert an address'
             ]);
         }
-
     }
 }
