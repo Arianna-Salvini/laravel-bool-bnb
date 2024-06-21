eb0c2e9c9ae1923564ac8b1857fccf2926c896dd
alessiavozzo <a.vozzo95@gmail.com>
2024-06-21 15:54:17 +0200

    change controller and form requests not to handle address details
    -client didnt pay enough to handle the not-existing address edge case

--- app/Http/Requests/StoreApartmentRequest.php
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -25,18 +25,18 @@ class StoreApartmentRequest extends FormRequest
             'title' => 'required|min:5|max:255',
             'user_id' => 'nullable|exists:users,id',
             'image' => 'nullable|image|max:1000',
-         'services' => 'required|array|min:1',//prima definisci la struttura
-            'services.*' => 'exists:services,id',//assegno ad ogni elemento dal array
+            'services' => 'required|array|min:1', //prima definisci la struttura
+            'services.*' => 'exists:services,id', //assegno ad ogni elemento dal array
             'description' => 'nullable|max:1000',
             'rooms' => 'nullable|integer|min:1',
             'beds' => 'nullable|integer|min:1',
             'bathrooms' => 'nullable|integer|min:1',
             'square_meters' => 'nullable|integer|min:1',
             'address' => 'required',
-            'street_number' => 'required|regex:/^[a-zA-Z0-9\/]+$/',
+            /* 'street_number' => 'required|regex:/^[a-zA-Z0-9\/]+$/',
             'country_code' => 'required',
             'zip_code' => 'required',
-            'city' => 'required',
+            'city' => 'required', */
             'visibility' => 'required|boolean',
         ];
     }
