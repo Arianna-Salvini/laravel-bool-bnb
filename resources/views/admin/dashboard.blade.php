1019e58c109af3431097932a6970d76ec869c3a8
Kasketto001 <kaskettomaledetto@gmail.com>
2024-06-21 17:18:03 +0200

    Fix micro details

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,7 +8,7 @@
 
         <div class="d-flex justify-content-between align-items-center">
             <h1>👋 Hi {{Auth::user()->name}}!</h1>
-            <a class="btn btn-primary btn-sm " href="http://localhost:5180/"   role="button" target="_BLANK">Search</a>
+            {{-- <a class="btn btn-primary btn-sm " href="http://localhost:5180/"   role="button" target="_BLANK">Search</a> --}}
         </div>
 
     
