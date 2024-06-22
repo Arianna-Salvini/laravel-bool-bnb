b749d3f1b762ef8b05944c4f336b79d32c78f534
alessiavozzo <a.vozzo95@gmail.com>
2024-06-22 02:36:09 +0200

    add working filter for services
    -use query builder
    -use subquery to get only results with given services
    -use haversine formula to calculate distance between 2 points
    -order results by distance

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ApartmentController extends Controller
 {
@@ -51,6 +52,7 @@ class ApartmentController extends Controller
 
         // Add range distance dynamically from range input in FE
         $range_distance = $request->input('range_distance', 20);
+        $services = $request->input('services', []);
 
         /* https://api.tomtom.com/search/2/geocode/Via%20degli%20anemoni%206%2000172%20Roma.json?storeResult=false&view=Unified&key=***** */
         //$api_url = $base_api.$address.'.json?storeResult=false&view=Unified&key='.$api_key;   
@@ -94,8 +96,53 @@ class ApartmentController extends Controller
             $long_min = ($longitude - $deg_radius / cos($rad_lat));
             $long_max = ($longitude + $deg_radius / cos($rad_lat));
 
-            $query = Apartment::query();
-            $apartments = $query->whereBetween('latitude', [$lat_min, $lat_max])->whereBetween('longitude', [$long_min, [$long_max]])->with('services', 'sponsorships', 'user')->orderByDesc('id')->paginate(4);
+            /* $query = Apartment::query(); */
+            /* $apartments = $query->whereBetween('latitude', [$lat_min, $lat_max])->whereBetween('longitude', [$long_min, [$long_max]])->with('services', 'sponsorships', 'user')->orderByDesc('id')->paginate(4); */
+            /* $query = $query->whereBetween('latitude', [$lat_min, $lat_max])->whereBetween('longitude', [$long_min, $long_max]);
+            if (!empty($services)) {
+                foreach ($services as $service) {
+                    $query->whereHas('services', function ($subquery) use ($service) {
+                        $subquery->whereIn('services.id', $service);
+                    });
+                }
+            }
+
+            $apartments = $query->with('services', 'sponsorships', 'user')->paginate(4); */
+
+
+            /* QUERY BUILDER VERSION */
+            /* try query builder to fetch apartments */
+            $query = DB::table('apartments')
+                ->select('apartments.*')
+                ->distinct()
+                ->join('apartment_service', 'apartments.id', '=', 'apartment_service.apartment_id')
+                ->join('services', 'apartment_service.service_id', '=', 'services.id')
+                ->whereBetween('apartments.latitude', [$lat_min, $lat_max])
+                ->whereBetween('apartments.longitude', [$long_min, $long_max]);
+
+            /* use subquery for each service */
+            foreach ($services as $service) {
+
+                /* if there is at least one record */
+                $query->whereExists(function ($subquery) use ($service) {
+                    $subquery
+                        ->from('apartment_service')
+                        ->where('apartment_service.service_id', $service);
+                });
+            }
+
+            /* order by distance from given address using haversine formula */
+            $query->select([
+                'apartments.*',
+                DB::raw("(
+                    2 * $earth_radius * ASIN (SQRT (POW (SIN((RADIANS($latitude - apartments.latitude)) / 2), 2) + COS (RADIANS($latitude)) * COS(RADIANS(apartments.latitude)) * POW(SIN ((RADIANS($longitude - apartments.longitude)) / 2), 2)
+                ))) AS distance")
+            ])
+                ->orderBy('distance', 'asc');
+
+            $apartments = $query->paginate(10);
+
+            /* recap: I use query builder and apply filter for latitude and longitude. I check if user checked at least one service and, if he did, I use a subquery for each service selected: I verify the record existence and take the records where service_id = $service  */
 
             return response()->json([
                 'success' => true,
