1ee22fbe69dbcb585f01619e2c0df25bca80b4dd
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-06-26 10:53:38 +0200

    add counter for messages, add accordion to show text message, fix layout

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -2,59 +2,100 @@
 
 @section('content')
     <div class="container">
-        <div class="row row-cols-2">
-            <div class="col d-flex gap-5">
+        <div class="d-flex mb-3">
+            <h4>Tot. Messaggi: </h4>
+            <h4 id="count" data-val="{{ $messages->count() }}" class="px-1"> 000</h4>
+        </div>
+        <div class="row">
+            <div class="col d-flex flex-column gap-3">
                 @forelse($messages as $message)
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">{{ $message->name }} {{ $message->lastname }}</h4>
-                            <p class="card-text">{{ $message->sender_email }}</p>
-                            <p class="card-text"><strong>Per l'appartamento: </strong>{{ $message->apartment->title }}</p>
-                            <a name="show" id="show" class="btn btn-dark text-light"
-                                href="{{ route('admin.messages.show', $message) }}" role="button"><i class="fa fa-eye"
-                                    aria-hidden="true"></i></a>
-                            <button type="button" class="btn btn-danger btn-sm btn-action" data-bs-toggle="modal"
-                                data-bs-target="#modalId-{{ $message->id }}">
-                                <i class="fa fa-trash" aria-hidden="true"></i>
+                            <div class="card-heading d-flex align-items-center justify-content-between">
+                                <div class="d-flex align-items-center gap-2">
+                                    <h4 class="card-title">{{ $message->name }} {{ $message->lastname }}</h4>
+                                    <p class="card-text">{{ $message->sender_email }}</p>
+                                </div>
 
-                            </button>
-                            <div class="modal fade" id="modalId-{{ $message->id }}" tabindex="-1"
-                                data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
-                                aria-labelledby="modalTitleId" aria-hidden="true">
-                                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md"
-                                    role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title" id="modalTitleId">
-                                                Attention! Are sure you want to delete the message of: {{ $message->name }}
-                                                {{ $message->lastname }} ?
-                                            </h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
+                                <p class="card-text"><strong>Sent: </strong>{{ $message->created_at }}</p>
+                            </div>
+                            <div class="card-info d-flex gap-3">
+                                <p class="card-text"><strong>Apartment: </strong>{{ $message->apartment->title }}</p>
+                            </div>
+
+                            <div class="accordion pb-3" id="accordionExample{{ $message->id }}">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="heading{{ $message->id }}">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                            data-bs-target="#collapse{{ $message->id }}" aria-expanded="true"
+                                            aria-controls="collapseOne" style="background-color: white">
+                                            Read the Message
+                                        </button>
+                                    </h2>
+                                    <div id="collapse{{ $message->id }}" class="accordion-collapse collapse"
+                                        aria-labelledby="heading{{ $message->id }}"
+                                        data-bs-parent="#accordionExample{{ $message->id }}">
+                                        <div class="accordion-body">
+                                            {{ $message->content }}
                                         </div>
-                                        <div class="modal-body">This is an irreversible operation.
-                                            Are you sure you want to run it?</div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                                                Close
-                                            </button>
-                                            <form action="{{ route('admin.message.destroy', $message) }}" method="POST">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="btn btn-danger">
-                                                    Confirm
-                                                </button>
+                                    </div>
+                                </div>
 
-                                            </form>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-end">
+                                <a name="show" id="show" class="btn btn-dark text-light"
+                                    href="{{ route('admin.messages.show', $message) }}" role="button"><i class="fa fa-eye"
+                                        aria-hidden="true" style="height: 10px"></i></a>
+                                <button type="button" class="btn btn-danger btn-sm btn-action" data-bs-toggle="modal"
+                                    data-bs-target="#modalId-{{ $message->id }}" style="height: 16px margin:auto">
+                                    <i class="fa fa-trash" style="font-size: 0.93rem" aria-hidden="true"></i>
+                                </button>
+                                <div class="modal fade" id="modalId-{{ $message->id }}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTitleId" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalTitleId">
+                                                    Attention! Are you sure you want to delete the message of:
+                                                    {{ $message->name }}
+                                                    {{ $message->lastname }} ?
+                                                </h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">This is an irreversible operation.
+                                                Are you sure you want to run it?</div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                    Close
+                                                </button>
+                                                <form action="{{ route('admin.message.destroy', $message) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">
+                                                        Confirm
+                                                    </button>
+                                                </form>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
-                    @empty
-                        <p>No messages</p>
+                    </div>
+
+                @empty
+                    <p>No messages</p>
                 @endforelse
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    @vite(['resources/js/message-count.js'])
 @endsection
