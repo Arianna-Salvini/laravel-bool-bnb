8eb827e91af1569e68e47c88c9e4f1de99ab08db
alessiavozzo <a.vozzo95@gmail.com>
2024-06-30 12:13:24 +0200

    remove dropdown menu on user name in navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -46,25 +46,11 @@
                     </li>
                 @endif
             @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle auth" href="#" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} {{ Auth::user()->lastname }}
-                    </a>
+                <div id="user" class="auth" v-pre>
+                    {{ Auth::user()->name }} {{ Auth::user()->lastname }}
+                </div>
 
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
             @endguest
         </ul>
     </div>
